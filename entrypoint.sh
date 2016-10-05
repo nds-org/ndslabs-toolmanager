38006ae44378d0b221818db02542bf1cbbd120a3
CoreOS Admin <core@willis8-dev-alpha-2.os.ncsa.edu>
2016-10-05 21:29:54 +0000

    Changed storage driver from vfs to overlay for performance

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ if [ "$1" = "toolserver" ]; then
    $(which dind) docker daemon \
         --host=unix:///var/run/docker.sock \
         --host=tcp://0.0.0.0:2375 \
-#        --registry-mirror=http://docker-cache.default:5001 \
-        --storage-driver=vfs  > dind.log 2>&1 &
+        --registry-mirror=http://docker-cache.default:5001 \
+        --storage-driver=overlay  > dind.log 2>&1 &
 
    toolserver
 
