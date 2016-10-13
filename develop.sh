6eaba8c8d7ff33bb7606eae30d42dcb52b424012
Mike Lambert <lambert8@illinois.edu>
2016-10-13 07:04:33 +0000

    Updated develop.sh and .gitignore

--- develop.sh
+++ develop.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 #
+# Grabs bower dependencies, then starts a container with the source mapped-in.
+#
 # Usage: docker build -t toolmanager . && ./develop.sh toolmanager
 #
 
-docker run -it --rm --name=toolmgr \
+docker run -it --rm -w /data \
+    -v `pwd`/js:/data \
+    bodom0015/nodejs-bower-grunt \
+    bower install
+
+docker run -it -d --name=toolmgr \
+    -p 8082-8083:8082-8083 \
     -v `pwd`/api/:/usr/local/bin/ \
     -v `pwd`/data/:/usr/local/data/ \
     -v `pwd`/js:/usr/share/nginx/html/ \
-    ${1:-ndslabs/toolmanager}
+    ${@:-ndslabs/toolmanager}
